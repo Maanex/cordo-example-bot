350ac8916b7c98ee6cdf840e866ab2b9bbc98050
maanex <maan91315@gmail.com>
2021-07-15 01:40:31 +0200

    no build folder

--- build/bot/components/cool/select.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(i) {
-    i.replyPrivately({
-        content: 'You picked ' + i.data.values[0]
-    });
-}
-exports.default = default_1;
-//# sourceMappingURL=select.js.map
--- build/bot/components/cool_button.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(i) {
-    i.ack();
-    console.log('this button doesnt do anything except for printing this line to the console');
-}
-exports.default = default_1;
-//# sourceMappingURL=cool_button.js.map
